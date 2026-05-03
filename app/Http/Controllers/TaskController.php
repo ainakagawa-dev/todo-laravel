4578d4e9134cfea1439416c8f9d135761ebd9a13
ainakagawa-dev <tetoteto200505@gmail.com>
2026-05-03 21:58:23 +0900

    DB接続+一覧をDBから取得に変更

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,30 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     //
     public function index()
     {
-        $tasks = [
-            ['title' => '風呂掃除', 'status' => '未着手'],
-            ['title' => '布団干し', 'status' => '未着手'],
-            ['title' => 'おむつ整理', 'status' => '着手中'],
-            ['title' => 'ゴミ捨て', 'status' => '完了'],
-        ];
+        $tasks = Task::all();
 
-        $todo = array_filter($tasks, function($task){
-        return $task['status'] === '未着手';
-    });
-
-    $doing = array_filter($tasks, function($task){
-        return $task['status'] === '着手中';
-    });
-
-    $done = array_filter($tasks, function($task){
-        return $task['status'] === '完了';
-    });
+        $todo = $tasks->where('status', '未着手');
+        $doing = $tasks->where('status', '着手中');
+        $done = $tasks->where('status', '完了');
 
         return view('tasks.index', compact('todo', 'doing', 'done'));
     }
